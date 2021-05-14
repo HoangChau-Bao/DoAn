78e124773f53e6ba4cef069a7d2b38bd8ab941dc
baohoang040200 <baohoang040200@gmail.com>
2021-05-14 20:01:52 +0700

    manager page by Hoang

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,10 +1,120 @@
 const config = require('../../config/db/dbconfig');
 const sql = require('mssql');
+const { text } = require('express');
 
 class AdminController {
   //[GET] /admin/productmanage
   productmanage(req, res) {
-    res.render('admin/productmanage');
+    sql.connect(config, (err, dienthoai) => {
+      let str = 'SELECT * FROM DienThoai';
+      let request = new sql.Request();
+      if (err) {
+        console.log('Error while querying database :- ' + err);
+        throw err;
+      } else {
+        request.query(str, function (err, dienthoai) {
+          if (err) {
+            console.log('ERROR ' + err);
+            throw err;
+          } else {
+            res.render('admin/productmanage', {
+              dienthoai: dienthoai.recordset,
+            });
+          }
+        });
+      }
+    });
+  }
+
+  //[POST] /admin/productmanage/deleteproduct
+  deleteproduct(req, res) {
+    console.log(req.body);
+    sql.connect(config, (err, dienthoai) => {
+      let str =
+        "DELETE FROM DienThoai WHERE DienThoaiId= '" +
+        req.body.DienThoaiId +
+        "';";
+      let request = new sql.Request();
+      if (err) {
+        console.log('Error while querying database :- ' + err);
+        throw err;
+      } else {
+        request.query(str, function (err, dienthoai) {
+          if (err) {
+            console.log('ERROR ' + err);
+            throw err;
+          } else {
+            res.redirect('/admin/productmanage');
+          }
+        });
+      }
+    });
+  }
+
+  //[GET] /admin/productmanage/addnewproductform
+  addform(req, res) {
+    res.render('admin/addproduct');
+  }
+
+  //[POST] /admin/productmanage/addnewproduct/store
+  store(req, res) {
+    //res.send(req.body.NgayRaMat);
+    sql.connect(config, (err, dienthoai) => {
+      let str =
+        'INSERT INTO DienThoai (TenDT,ManHinh,CameraSau,CameraSelfie,Ram,BoNhoTrong,CPU,GPU,' +
+        'DungLuongPin,TheSim,HeDieuHanh,XuatSu,NgayRaMat,Gia,Slug,ImageLink,MoTa,TrangThaiKinhDoanh) ' +
+        "VALUES (N'" +
+        req.body.TenDT +
+        "', N'" +
+        req.body.ManHinh +
+        "', N'" +
+        req.body.CameraSau +
+        "', N'" +
+        req.body.CameraSelfie +
+        "', " +
+        "N'" +
+        req.body.Ram +
+        "', N'" +
+        req.body.BoNhoTrong +
+        "', N'" +
+        req.body.CPU +
+        "', N'" +
+        req.body.GPU +
+        "', N'" +
+        req.body.DungLuongPin +
+        "', " +
+        "N'" +
+        req.body.TheSim +
+        "', N'" +
+        req.body.HeDieuHanh +
+        "', N'" +
+        req.body.XuatSu +
+        "', '" +
+        req.body.NgayRaMat +
+        "', '" +
+        req.body.Gia +
+        "', " +
+        "N'" +
+        req.body.ManHinh +
+        req.body.DienThoaiId +
+        "', N'ImageLink', N'" +
+        req.body.MoTa +
+        "', 'true');";
+      let request = new sql.Request();
+      if (err) {
+        console.log('Error while querying database :- ' + err);
+        throw err;
+      } else {
+        request.query(str, function (err, dienthoai) {
+          if (err) {
+            console.log('ERROR ' + err);
+            throw err;
+          } else {
+            res.redirect('/admin/productmanage');
+          }
+        });
+      }
+    });
   }
 }
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const adminController = require('../app/controllers/AdminController');
 
 router.get('/productmanage', adminController.productmanage);
+router.post('/productmanage/deleteproduct', adminController.deleteproduct);
+router.get('/productmanage/addnewproduct', adminController.addform);
+router.post('/productmanage/addnewproduct/store', adminController.store);
 
 module.exports = router;
